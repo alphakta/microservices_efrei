fc9b4b7aced670f6e938ff4be9ff7a8509b478dd
alphakta <alpha94.k@gmail.com>
2023-06-08 11:01:58 +0000

    delete script run

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd authors-service
-npm install
-cd ../books-service
-npm install
-cd ../authors-service
-npm install
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd authors-service
-nodemon index.js &
-cd ../books-service
-nodemon index.js &
-cd ../authors-service
-nodemon index.js &
